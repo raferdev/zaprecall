381982fbd39aef2e0b070b0d41e216164315f711
Rafael <rafinha_fern@hotmail.com>
2022-03-17 04:29:35 -0300

    primeiro layout seguindo o figma

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import ReactDom from "react-dom";
-const appHtml = document.querySelector(".root")
+const appHtml = document.getElementById("root")
 function App() {
     return (
-        <div className="app">
-        <h1>HELLO WORLD!</h1>
+        <div id="app">
+            <main>
+                <div className="content">
+                <img className="logo" src="./svg/logo.svg" alt="logo-light"></img>
+                <h1>ZapRecall</h1>
+                <div className="initial-buttom"><p>Iniciar Recall!</p></div>
+                </div>
+            </main>
         </div>
     )
 }
