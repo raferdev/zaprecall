7c29ace893941d18800596f4487e471b6612c027
Rafael <rafinha_fern@hotmail.com>
2022-03-17 16:49:31 -0300

    regra de aparecimento da pagina principal

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import ReactDom from "react-dom";
-const appHtml = document.getElementById("root")
-function App() {
+import React from "react";
+const appHtml = document.getElementById("root");
+export default function App() {
+   const [visible, setVisible] = React.useState(true);
     return (
         <div id="app">
-            <main>
-                <div className="content">
-                <img className="logo" src="./svg/logo.svg" alt="logo-light"></img>
-                <h1>ZapRecall</h1>
-                <div className="initial-buttom"><p>Iniciar Recall!</p></div>
-                </div>
-            </main>
+            {visible? (
+                  <main>
+                  <div className="content">
+                      <img className="logo" src="./svg/logo.svg" alt="logo-light"></img>
+                      <h1>ZapRecall</h1>
+                      <div className="initial-buttom" onClick={() => setVisible(false)}><p>Iniciar Recall!</p></div>
+                  </div>
+              </main>
+            ): <></>}
         </div>
     )
 }
