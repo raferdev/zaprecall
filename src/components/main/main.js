327cc447480809cc276bc044c28bf9a40ca762f4
Rafael <rafinha_fern@hotmail.com>
2022-03-21 00:47:36 -0300

    botoes nos cards de resposta

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -10,7 +10,7 @@ export default function Main() {
             deck.map((deck,index) => {
             const  conteudo = answare? deck.answare:deck.quest
              return actvQuest === index? (
-                <article key={index} className="quest" onClick={() => setAnsware(()=>answare?false:true)}><h2>{conteudo}</h2><img className="turn" src={turn} alt="turn"/></article>
+                <article key={index} className="quest" onClick={() => setAnsware(()=>answare?false:true)}><h2>{conteudo}</h2><div className="answare-rate"><div className="dont-remember"><h2>Não lembrei</h2></div><div className="dont-know"><h2>Não lembrei</h2></div><div className="remember"><h2>Não lembrei</h2></div></div></article>
            ) : ( 
                 <article key={index} className="initial-quest" onClick={()=>setActvQuest(index)}><h2>Pergunta {index + 1}</h2><ion-icon name="play-outline"></ion-icon></article>
                 )
@@ -18,7 +18,8 @@ export default function Main() {
         </main>
     )};
 
-    /* <article onClick=""><h2 class="">Pergunta 1</h2><ion-icon name="play-outline"></ion-icon></article>
+/*     <img className="turn" src={turn} alt="turn"/>
+ */    /* <article onClick=""><h2 class="">Pergunta 1</h2><ion-icon name="play-outline"></ion-icon></article>
             <article><h2 class="">Pergunta 2</h2><ion-icon name="play-outline"></ion-icon></article>
             <article><h2 class="">Pergunta 3</h2><ion-icon name="play-outline"></ion-icon></article>
             <article><h2 class="">Pergunta 4</h2><ion-icon name="play-outline"></ion-icon></article> */
