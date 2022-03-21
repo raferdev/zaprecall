47b535e3fa51aeeb18ee6089b6588d8607fb58c6
Rafael <rafinha_fern@hotmail.com>
2022-03-21 16:28:59 -0300

    adicionando sort()

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,14 +1,11 @@
 import { deck } from "../../data/deck"
 import { Render } from "./render"
 export default function Main(props) {
-    const {redActv,setPoints} = props
-    function func(a, b) {  
-        return 0.5 - Math.random();
-      }  
+    const {redActv,setPoints} = props;
     return ( 
         <main>
             {
-            deck.sort(func).map((deck,index) => <Render key={index} deck={deck}index={index} redActv={redActv} setPoints={setPoints}/>)
+            deck.sort().map((deck,index) => <Render key={index} deck={deck}index={index} redActv={redActv} setPoints={setPoints}/>)
             }
         </main>
     )};
