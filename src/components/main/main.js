2da110a7d2b75b66c1b1468ba221492a9fb132d6
Rafael <rafinha_fern@hotmail.com>
2022-03-21 15:10:10 -0300

    resolvendo problemas com deploy

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,10 +2,13 @@ import { deck } from "../../data/deck"
 import { Render } from "./render"
 export default function Main(props) {
     const {redActv,setPoints} = props
+    function func(a, b) {  
+        return 0.5 - Math.random();
+      }  
     return ( 
         <main>
             {
-            deck.sort().map((deck,index) => <Render key={index} deck={deck}index={index} redActv={redActv} setPoints={setPoints}/>)
+            deck.sort(func).map((deck,index) => <Render key={index} deck={deck}index={index} redActv={redActv} setPoints={setPoints}/>)
             }
         </main>
     )};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDom from "react-dom";
 import React from "react";
 import InitialPage from "./components/initialPage/InitialPage";
-import resetCSS from "./styles/reset.css";
-import styleCSS from "./styles/style.css";
+import "./styles/reset.css";
+import "./styles/style.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/footer";
 import Main from "./components/main/main";
