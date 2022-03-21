ccecf1871cd681de220a62b79c335f60b7e6634e
Rafael <rafinha_fern@hotmail.com>
2022-03-20 22:26:34 -0300

    Renderizar perguntas aleatorias escondidas, e abertas ao clicar

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,10 +1,21 @@
+import { deck } from "../../data/deck"
+import React from "react"
 export default function Main() {
-    return (
+    const [actvQuest, setActvQuest] = React.useState(-1)
+    return ( 
         <main>
-            <article><h2>Pergunta 1</h2><ion-icon name="play-outline" class="play-outline"></ion-icon></article>
-            <article><h2>Pergunta 2</h2><ion-icon name="play-outline" class="play-outline"></ion-icon></article>
-            <article><h2>Pergunta 3</h2><ion-icon name="play-outline" class="play-outline"></ion-icon></article>
-            <article><h2>Pergunta 4</h2><ion-icon name="play-outline" class="play-outline"></ion-icon></article>
+            {
+            deck.map((deck,index) => {
+             return actvQuest === index? (
+                <article key={index}><h2 className="">{deck.pergunta}</h2><ion-icon name="play-outline"></ion-icon></article>
+            ) : ( 
+                <article key={index} onClick={()=>setActvQuest(index)}><h2 className="">Pergunta {index}</h2><ion-icon name="play-outline"></ion-icon></article>
+                )
+            })}
         </main>
-    )
-}
+    )};
+
+    /* <article onClick=""><h2 class="">Pergunta 1</h2><ion-icon name="play-outline"></ion-icon></article>
+            <article><h2 class="">Pergunta 2</h2><ion-icon name="play-outline"></ion-icon></article>
+            <article><h2 class="">Pergunta 3</h2><ion-icon name="play-outline"></ion-icon></article>
+            <article><h2 class="">Pergunta 4</h2><ion-icon name="play-outline"></ion-icon></article> */
---
+++ src/data/deck.js
@@ -0,0 +1,25 @@
+export const deck =  [{
+    pergunta:"O que é JSX?",
+    reposta:"Uma extensão de linguagem do JavaScript."
+},{
+    pergunta:"O React é __",
+    resposta:"Uma biblioteca JavaScript para construção de interfaces."
+}, {
+    pergunta:"Componentes devem iniciar com __ ",
+    resposta:"Letra maiúscula."
+}, {
+    pergunta:"Podemos colocar __ dentro do JSX",
+    reposta:"Expressões."
+},{
+    pergunta:"O ReactDOM nos ajuda __",
+    resposta:"Interagindo com a DOM para colocar componentes React na mesma."
+}, {
+    pergunta:"Usamos o npm para __",
+    resposta:"Gerenciar os pacotes necessários e suas dependências."
+}, {
+    pergunta:"Usamos props para __",
+    resposta:"Passar diferentes informações para componentes."
+}, {
+    pergunta:"Usamos estado (state) para __",
+    resposta:"Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente."
+}];
